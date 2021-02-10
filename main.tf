cad6f6d0af0d699dd9a0e7440c81d05cf364e63c
Idriss Charai <idrisscharai@gmail.com>
2021-02-10 11:14:14 +0000

    attached policies to groups

--- main.tf
+++ main.tf
@@ -17,11 +17,20 @@ resource "aws_iam_group" "students" {
 
 ##### ADDING A DEFAULT POLICY TO THE GROUPS #####
 
-#resource "aws_iam_policy_attachment" "group-policy-attach" {
-#    name = "group-policy-attach"
-#    groups = ["${aws_iam_group.trainers.name}", "${aws_iam_group.students.name}"]
-#    policy_arn = "arn:aws:iam::aws:policy/ADD_POLICY_NAME"
-#}
+resource "aws_iam_policy_attachment" "trainer_group_attach" {
+  name = "trainer_group_attach"
+  for_each = toset(var.trainer_policies)
+  groups = [aws_iam_group.trainers.name]
+  policy_arn = each.value
+}
+
+resource "aws_iam_policy_attachment" "student_group_attach" {
+  name = "student_group_attach"
+  for_each = toset(var.student_policies)
+  groups = [aws_iam_group.students.name]
+  policy_arn = each.value
+}
+
 
 ##### CREATING THE TRAINER USERS #####
 
@@ -40,16 +49,18 @@ resource "aws_iam_user" "student" {
 ##### ADDING USERS TO THEIR GROUPS #####
 
 resource "aws_iam_group_membership" "trainers_membership" {
-    name = "Trainer Memberships"
-    for_each = toset(var.trainer_users)
-    users = [aws_iam_user.trainer[each.value].name]
-    group = aws_iam_group.trainers.name
+  name = "Trainer Memberships"
+  for_each = toset(var.trainer_users)
+  users = [aws_iam_user.trainer[each.value].name]
+  group = aws_iam_group.trainers.name
+  depends_on = [aws_iam_user.trainer, aws_iam_group.trainers]
 }
 
 resource "aws_iam_group_membership" "students_membership" {
-    name = "Student Memberships"
-    for_each = toset(var.student_users)
-    users = [aws_iam_user.student[each.value].name]
-    group = aws_iam_group.students.name
+  name = "Student Memberships"
+  for_each = toset(var.student_users)
+  users = [aws_iam_user.student[each.value].name]
+  group = aws_iam_group.students.name
+  depends_on = [aws_iam_user.student, aws_iam_group.students]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+##### USERS #####
+
 variable "trainer_users" {
   type = list(string)
   default = ["Sabir", "Darina", "Shahid"] 
@@ -8,3 +10,14 @@ variable "student_users" {
   default = ["Umar", "Janis_K", "Janis_R", "Idriss", "Agnese"]
 }
 
+##### POLICIES #####
+
+variable "trainer_policies" {
+  type = list(string)
+  default = ["arn:aws:iam::aws:policy/AmazonVPCFullAccess", "arn:aws:iam::aws:policy/AmazonEC2FullAccess", "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"]
+}
+
+variable "student_policies" {
+  type = list(string)
+  default = ["arn:aws:iam::aws:policy/AmazonVPCFullAccess", "arn:aws:iam::aws:policy/AmazonEC2FullAccess", "arn:aws:iam::aws:policy/AWSCloudFormationFullAccess"]
+}
